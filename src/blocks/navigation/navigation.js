e58ef8bbc3a2dc5094e505de6a9b7c483c33ee59
Hello-Kozloff <hello.kozloff@gmail.com>
2019-09-13 17:45:49 +0300

    Added hide status when click right section

--- src/blocks/navigation/navigation.js
+++ src/blocks/navigation/navigation.js
@@ -1,10 +1,19 @@
 $(document).ready(() => {
+    $(".navigation").on('click', function(event) {
+        if (event.target !== this) return false;
+
+        hideNavigationPanel();
+    });
+
     $(".navigation__button").on("click", function (event) {
         event.preventDefault();
-
-        const _navigation = $(".navigation");
-        _navigation.toggleClass("navigation_hidden");
-
-        $("body").removeClass("fixed");
+        hideNavigationPanel();
     });
 });
+
+function hideNavigationPanel() {
+    const _navigation = $(".navigation");
+    _navigation.toggleClass("navigation_hidden");
+
+    $("body").removeClass("fixed");
+}
