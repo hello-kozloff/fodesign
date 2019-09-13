b4160ed5933ca77974d8c1cc3fc30e2800b1872b
Hello-Kozloff <hello.kozloff@gmail.com>
2019-09-13 12:06:42 +0300

    Created services-carousel component

---
+++ src/blocks/services-carousel/services-carousel.js
@@ -0,0 +1,52 @@
+$(document).ready(() => {
+    let $services = {};
+
+    $services.slider =  $(".services-carousel__categories-slider").flickity({
+        fade: true,
+        pageDots: false,
+        draggable: false,
+        prevNextButtons: false
+    });
+
+    //- Get categories and single category
+    $services.categories = $(".services-carousel__categories");
+    $services.category = $(".services-carousel__item");
+
+    console.log($services.category)
+
+    //- On click single category
+    $services.category.on("click", function (event) {
+        const idx = $(event.currentTarget).index();
+        $services.slider.flickity("select", idx);
+    });
+
+    const _flickity = $services.slider.data("flickity");
+    $services.height = $services.categories.height();
+    $services.categoriesHeight = $services.categories.height();
+
+    $services.slider.on("select.flickity", function () {
+        //- Remove old current item
+        $services.categories
+            .find(".services-carousel__item_current")
+            .removeClass("services-carousel__item_current");
+
+        const selected = $services.category
+            .eq(_flickity.selectedIndex)
+            .addClass("services-carousel__item_current");
+
+        const scrollY = selected.position().top +
+            $services.categories.scrollTop() - ($services.height + $services.categoriesHeight) / 2;
+
+        $services.categories.animate({
+            scrollTop: scrollY
+        });
+    });
+});
+
+$(document).ready(() => {
+    $(".services-carousel__category-slider").flickity({
+        pageDots: true,
+        draggable: false,
+        prevNextButtons: false
+    });
+});
