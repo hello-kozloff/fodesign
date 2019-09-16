af71314077ba23dc9b1f90da2bc42fa716d9276e
Hello-Kozloff <hello.kozloff@gmail.com>
2019-09-16 16:21:28 +0300

    <Feature>: Added wrap around in clients-carousel

--- src/blocks/clients-carousel/clients-carousel.js
+++ src/blocks/clients-carousel/clients-carousel.js
@@ -6,7 +6,8 @@ $(document).ready(() => {
         pageDots: false,
         draggable: false,
         prevNextButtons: false,
-        adaptiveHeight: true
+        adaptiveHeight: true,
+        wrapAround: true
     });
 
     //- On click prev controller
